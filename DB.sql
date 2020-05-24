d89d166c9563ab4dc973450152b6d385437b20b8
sachin shajan vettuvelil <sachinshajanvettuvelil@gmail.com>
2020-05-24 19:33:45 +0530

    Online shopping cart using PHP
    This cart automatically adds and subtracts the amount as per the products are added in the cart

---
+++ DB.sql
@@ -0,0 +1,5 @@
+INSERT INTO `producttb` (`id`, `product_name`, `product_price`, `product_image`) VALUES
+(1, 'Samsung galaxy A50', 1799, './upload/product1.png'),
+(2, 'One Plus 7T Pro', 147, './upload/product2.png'),
+(3, 'Oppo A57', 459, './upload/product3.png'),
+(4, 'Sennheiser Headphone', 278, './upload/product4.png');
---
+++ README.txt
@@ -0,0 +1,4 @@
+1.Open phpMyAdmin,
+2.from the left side dropdown,select the ‘productdb’ 
+3.and into the ‘productdb’ database  schema IMPORT the database file(DB.sql) 
+4.Now run the project folder from your local host 
